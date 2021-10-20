7ed5aa8a1f4546bbce29b1883d772b298a64feba
Nikolas-Pensyl <nik.pensyl@aol.com>
2021-10-20 08:51:29 -0400

    Computer IP List Widget

--- IT_Management/startwidget.cpp
+++ IT_Management/startwidget.cpp
@@ -3,21 +3,57 @@
 
 //Run winbdoiwremnote.exe
 using namespace std;
+using namespace std::experimental::filesystem;
+
 StartWidget::StartWidget(QWidget *parent) : QWidget(parent) {
     //Create tabs
     QTabWidget* tab;
     tab = new QTabWidget(this);
     IPlist_widget = new QTabWidget;
     settings_widget = new QTabWidget;
-    settings_widget->setStyleSheet("background-color:red;");
     login_widget = new QTabWidget;
-    login_widget->setStyleSheet("background-color:blue;");
 
+
+
+    //Set the tabs to the Screen
+    login();
+    CompsbyIP();
+    tab->addTab(IPlist_widget, "Computers");
+    tab->addTab(settings_widget, "Settings");
+    //tab->addTab(login_widget, "login");
+    tab->setFixedSize(700, 500);
+}
+
+StartWidget::~StartWidget()
+{
+}
+
+void StartWidget::CompsbyIP() {
     //Scan LAN
     LAN = ScanLAN();
     //Set buttons for IPs scanned on LAN
-    myHLayouts.push_back(new QHBoxLayout);
     fullLayout = new QVBoxLayout;
+    QHBoxLayout *labs = new QHBoxLayout();
+    QLabel *label = new QLabel(IPlist_widget);
+    label->setText("When a button is clicked, run command this command: ");
+    labs->addWidget(label);
+    labs->addSpacing(10);
+    myHLayouts.push_back(new QHBoxLayout);
+    QComboBox *comm = new QComboBox(IPlist_widget);
+    comm->addItems(commands);
+    labs->addWidget(comm);
+    labs->addSpacing(10);
+    QPushButton *butt_all = new QPushButton();
+    butt_all->setText("Run command for all");
+    labs->addWidget(butt_all);
+
+
+    QSignalMapper* sigMapper = new QSignalMapper (this) ;
+    connect(butt_all, SIGNAL(clicked()), sigMapper, SLOT(map()));
+    sigMapper->setMapping(butt_all, comm->currentText());
+    connect(sigMapper, SIGNAL(mapped(QString)), this, SLOT(runAll(QString)));
+
+    fullLayout->addLayout(labs);
     QSignalMapper* signalMapper = new QSignalMapper (this) ;
     int i = 0, num =0;
     for(std::vector<string>::iterator lss = LAN.begin(); lss!=LAN.end(); lss++, i++) {
@@ -37,18 +73,81 @@ StartWidget::StartWidget(QWidget *parent) : QWidget(parent) {
     connect (signalMapper, SIGNAL(mapped(QString)), this, SLOT(execut(QString))) ;
     fullLayout->addLayout(myHLayouts[num]);
     IPlist_widget->setLayout(fullLayout);
-
-    //Set the tabs to the Screen
-
-    tab->addTab(settings_widget, "Settings");
-    tab->addTab(login_widget, "login");
-    tab->addTab(IPlist_widget, "Computers by IP");
-    tab->setFixedSize(700, 500);
 }
 
-StartWidget::~StartWidget()
-{
+void StartWidget::runAll(QString cmd) {
+    string com = cmd.toStdString();
+     for(std::vector<string>::iterator lss = LAN.begin(); lss!=LAN.end(); lss++) {
+         exec(com + *lss);
+     }
 }
+
+void StartWidget::login() {
+    string user, passw, txt;
+    QVBoxLayout *login_layout = new QVBoxLayout();
+    QHBoxLayout *User_lay = new QHBoxLayout();
+    QHBoxLayout *Pass_lay = new QHBoxLayout();
+    QLabel *label = new QLabel(login_widget);
+    QLabel *user_label = new QLabel(login_widget);
+    QLabel *pass_label = new QLabel(login_widget);
+    QLineEdit *usern = new QLineEdit();
+    QLineEdit *passwo = new QLineEdit();
+    QPushButton *login_button = new QPushButton("Login", login_widget);
+    label->setText("Login in with the details you previously used");
+
+    user_label->setText("Username:");
+    pass_label->setText("Password:");
+
+    User_lay->addWidget(user_label);
+    User_lay->addSpacing(10);
+    User_lay->addWidget(usern);
+
+    Pass_lay->addWidget(pass_label);
+    Pass_lay->addSpacing(10);
+    Pass_lay->addWidget(passwo);
+
+    login_layout->addWidget(label);
+    login_layout->addSpacing(10);
+    login_layout->addLayout(User_lay);
+    login_layout->addSpacing(10);
+    login_layout->addLayout(Pass_lay);
+    login_layout->addSpacing(10);
+    login_layout->addWidget(login_button);
+    login_widget->setLayout(login_layout);
+}
+   /* fstream file;
+    file.open("logininfo.txt");
+    if(std::experimental::filesystem::exists("logininfo.txt")) {
+        label->setText("Login in with the details you previously used");
+        ifstream infile;
+        infile.open("logininfo.txt");
+        while(getline(infile, txt)) {
+            if(txt.find("username:")!=string::npos) {
+                user = txt.substr(9, txt.length());
+            } else if(txt.find("password:")!=string::npos) {
+                passw = txt.substr(9, txt.length());
+            }
+        }
+        if(usern->text().toStdString().compare(user)==0) {
+            if(passwo->text().toStdString().compare(passw)==0) {
+                logged = true;
+            }
+        }
+        infile.close();
+    } else {
+
+        label->setText("You are setting your username and password for the next time you sign in.");
+        ofstream ofile;
+        ofile.open("logininfo.txt");
+        ofile<<"username:"<<usern->text().toStdString();
+        ofile<<"password:"<<passwo->text().toStdString();
+        ofile.close();
+        logged = true;
+    }
+    login_layout->addWidget(label);
+    login_layout->addWidget(usern);
+    login_layout->addWidget(passwo);
+}*/
 
 void StartWidget::execut(QString cmd) {
     string com = cmd.toStdString();
@@ -76,6 +175,20 @@ string StartWidget::exec(string command) {
     }
 
     _pclose(pipe);
+
+    time_t now = time(0);
+    char* dt = ctime(&now);
+
+    ofstream ofile;
+    ofile.open("../Outlog.txt", std::ios_base::app);
+
+    ofile<<dt<<result<<endl<<endl<<endl;
+    ofile.close();
+
+    ofstream ifile;
+    ifile.open("../Inlog.txt", std::ios_base::app);
+    ifile<<dt<<command<<endl<<endl<<endl;
+    ifile.close();
     return result;
  }
 
--- IT_Management/startwidget.h
+++ IT_Management/startwidget.h
@@ -7,11 +7,17 @@
 #include <stdexcept>
 #include <stdio.h>
 #include <string>
+#include <QTimer>
 #include <sstream>
 #include <vector>
 #include <QPushButton>
 #include <QHBoxLayout>
 #include <QSignalMapper>
+#include <QLabel>
+#include <fstream>
+#include <QLineEdit>
+#include <ctime>
+#include <QComboBox>
 
 using namespace std;
 class StartWidget : public QWidget
@@ -25,15 +31,20 @@ public:
     vector<string> ScanLAN();
     vector<QPushButton*> comps;
     vector<QHBoxLayout*> myHLayouts;
+    QStringList commands = {"ping ", "tracert"};
     QVBoxLayout *fullLayout;
+    void login();
+    void CompsbyIP();
 
 private:
     QTabWidget* IPlist_widget;
     QTabWidget* login_widget;
     QTabWidget* settings_widget;
     vector<string> LAN;
+    bool logged = false;
 
 public slots:
     void execut(QString cmd);
+    void runAll(QString cmd);
 };
 #endif // STARTWIDGET_H
